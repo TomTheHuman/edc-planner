0b262ae462fdc26b05d147e657cb612c5581ba1c
Thomas Shaw <tshaw@PF2S8EET.corp.astranis.com>
2023-05-05 06:27:57 -0700

    Fixed bad data reference

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import React, { useEffect } from 'react';
 import Image from 'next/image';
 
 // Internal Imports
-import { landing, lineup } from '../utils/Info';
+import { landing } from '../utils/Info';
 import sx from '../styles/pages/Landing.module.scss';
 
 /**
@@ -14,44 +14,6 @@ import sx from '../styles/pages/Landing.module.scss';
  */
 export default function Landing(): JSX.Element {
   const { text } = landing;
-
-  function calc() {
-    const newArr: any[] = [];
-    lineup.forEach((a) => {
-      const sets = a.days.map((d) => {
-        switch (d) {
-          case 'friday':
-            return {
-              start: new Date('2023-05-20T03:00:00.000+00:00'),
-              end: new Date('2023-05-20T04:00:00.000+00:00'),
-            };
-          case 'saturday':
-            return {
-              start: new Date('2023-05-21T03:00:00.000+00:00'),
-              end: new Date('2023-05-21T04:00:00.000+00:00'),
-            };
-          case 'sunday':
-            return {
-              start: new Date('2023-05-22T03:00:00.000+00:00'),
-              end: new Date('2023-05-22T04:00:00.000+00:00'),
-            };
-          default:
-            return null;
-        }
-      });
-      const obj = {
-        name: a.artist,
-        stages: a.stage,
-        sets,
-      };
-      newArr.push(obj);
-    });
-    console.log(newArr);
-  }
-
-  useEffect(() => {
-    calc();
-  }, []);
 
   return (
     <div className={sx.root}>
